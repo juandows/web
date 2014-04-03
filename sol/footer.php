b9be86ed47b1a626e12d63c2a1397ef15503b51b
juande <juande@juande-VirtualBox.(none)>
2014-04-03 12:56:14 +0200

    Eliminados archivos sobrantes

--- sol/footer.php
+++
@@ -1,9 +0,0 @@
-	<footer>
-	  <p>&copy; Juandows 2014</p>
-	</footer>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/js/bootstrap.min.js"></script>
-  </body>
-</html>
